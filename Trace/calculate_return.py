c9a8aca7718353ee81b9aabdfd5f03d9c3e83e4a
Dylan Arone <daarone@my.waketech.edu>
2024-02-09 17:45:10 -0500

    Update calculate_return.py
    Added code stub and docstring to calculate_return.py

--- Trace/calculate_return.py
+++ Trace/calculate_return.py
@@ -3,8 +3,17 @@
 Contains function(s) for calculating and returning the user's tax returns based on user input as parameters.
 """
 
-def calculate_state_return():
+def calculate_state_return(forms_w2, forms_1099):
     """
-    :param:
-    :return: state_return
+    :param forms_w2: list composed of dictionary objects that represent W-2 forms.
+    :param forms_1099: list composed of dictionary objects that represent 1099 forms.
+    :return: value of refund or value of additional taxes due.
+    Function to calculate state tax returns.
     """
+    for form in forms_w2:
+        # Loop through list of W-2 forms and total up any required values.
+
+    for form in forms_1099:
+        # Loop through list of 1099 forms and total up any required values.
+
+    pass
