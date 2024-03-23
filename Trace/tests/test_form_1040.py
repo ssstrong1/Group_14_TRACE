5360e1047b6f7106ec137ab55ca8bbfea8d7b997
Dylan Arone <daarone@my.waketech.edu>
2024-03-23 11:42:55 -0400

    Update test_form_1040.py
    Added stubs for pytest fixtures

--- Trace/tests/test_form_1040.py
+++ Trace/tests/test_form_1040.py
@@ -6,4 +6,28 @@ Pytest unit testing for form_1040.py
 
 import pytest
 
-import form_1040
+
+@pytest.fixture
+def setup_form_w2():
+    """
+    Fixture setup, initialize instance of form W-2
+    :return: Form W-2 test data
+    """
+    pass
+
+@pytest.fixture
+def setup_form_1099_div():
+    """
+    Fixture setup, initialize instance of form 1099-DIV
+    :return: Form 1099-DIV test data
+    """
+    pass
+
+@pytest.fixture
+def setup_form_1040(setup_form_w2, setup_form_1099_div):
+    """
+    Fixture setup, initialize instance of form 1040
+    :return: Form 1040 test data
+    """
+    pass
+
